3eefcaf64e2f461af8dbbd2b2f5fbe23924b4352
PRG Admin <prgadmin@Chemicals-MacBook-Pro.local>
2020-11-05 18:20:57 -0800

    v1

---
+++ beto_api/app/forms.py
@@ -0,0 +1,38 @@
+from app.models import User
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, RadioField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
+class LabelForm(FlaskForm):
+    radio = RadioField('Label', choices=[('relevant','Relevant'),
+                                         ('irrelevant', 'Irrelevant'),
+                                         ('idk', "I'm Not Sure")])
+    submit = SubmitField('Classify')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('This username is already in use.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('This email address is already in use.')
+        email_server = email.data.split('.')[-1]
+        if email_server != 'edu':
+            raise ValidationError('Please register with a .edu email address')
