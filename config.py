fced31a1a4717d41a335c64402995b4200c81f91
PRG Admin <prgadmin@Chemicals-MacBook-Pro.local>
2020-11-06 16:15:22 -0800

    config setup

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ['SECRET_KEY'] or 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'blank'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
     DEBUG = False
