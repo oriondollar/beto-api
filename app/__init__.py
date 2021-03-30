66800f4c75775a86e2fb47db6592f1aced91dd25
Orion Dollar <orion.dollar@gmail.com>
2021-03-30 16:28:06 -0700

    hidden_config

--- app/__init__.py
+++ app/__init__.py
@@ -10,12 +10,11 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.register_blueprint(api_bp, url_prefix='/api')
 hidden_config = {}
-with open('../.hidden_config.txt', 'r') as f:
+with open('.hidden_config.txt', 'r') as f:
     for line in f:
         line = line.split(':')
         hidden_config[line[0]] = line[1].split('\n')[0]
-mongodb =
-MongoClient('mongodb+srv://{}:{}@beto-corpora.cljbt.mongodb.net/Corpus?retryWrites=true&w=majority'.format(hidden_config['DATABASE_USERNAME'], hidden_config['DATABASE_PASSWORD'])).beto_corpora
+mongodb = MongoClient('mongodb+srv://{}:{}@beto-corpora.cljbt.mongodb.net/Corpus?retryWrites=true&w=majority'.format(hidden_config['DATABASE_USERNAME'], hidden_config['DATABASE_PASSWORD'])).beto_corpora
 # db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
 login = LoginManager(app)
