d9d6a4178870918a83f1c3f0512f6f514c2b1800
Orion Dollar <orion.dollar@gmail.com>
2021-04-06 16:40:24 -0700

    explore

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,7 +6,8 @@ with open('.hidden_config.txt', 'r') as f:
     for line in f:
         line = line.split(':')
         hidden_config[line[0]] = line[1].split('\n')[0]
-mongodb = MongoClient('mongodb+srv://{}:{}@beto-corpora.cljbt.mongodb.net/Corpus?retryWrites=true&w=majority'.format(hidden_config['DATABASE_USERNAME'], hidden_config['DATABASE_PASSWORD'])).beto_corpora
+mongodb = MongoClient('localhost', 27017).beto_corpora
+# mongodb = MongoClient('mongodb+srv://{}:{}@beto-corpora.cljbt.mongodb.net/Corpus?retryWrites=true&w=majority'.format(hidden_config['DATABASE_USERNAME'], hidden_config['DATABASE_PASSWORD'])).beto_corpora
 bp = Blueprint('api', __name__)
 
 from app.api import routes
