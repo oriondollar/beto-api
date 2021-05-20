d10708b618f264c6d426246d358fc2dbfc8433c3
oriondollar <orion.dollar@mgail.com>
2021-05-20 16:59:15 -0700

    rm hidden config

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.register_blueprint(api_bp, url_prefix='/api')
-hidden_config = {}
-with open('.hidden_config.txt', 'r') as f:
-    for line in f:
-        line = line.split(':')
-        hidden_config[line[0]] = line[1].split('\n')[0]
 mongodb = MongoClient('localhost', 27017).beto_corpora
-# mongodb = MongoClient('mongodb+srv://{}:{}@beto-corpora.cljbt.mongodb.net/Corpus?retryWrites=true&w=majority'.format(hidden_config['DATABASE_USERNAME'], hidden_config['DATABASE_PASSWORD'])).beto_corpora
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
